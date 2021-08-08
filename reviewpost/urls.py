67a829188cb5b6452b1011b4bf2bfeaaffc7636f
yopipi <ysk0420kbt@gmail.com>
2021-08-08 20:00:32 +0900

    first commit

---
+++ reviewpost/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib import admin
+from .views import signupview, loginview, listview, detailview, CreateClass, logoutview, evaluationview
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('signup/', signupview, name='signup'),
+    path('login/', loginview, name='login'),
+    path('list/', listview, name='list'),
+    path('detail/<int:pk>/', detailview, name='detail'),
+    path('create/', CreateClass.as_view(), name='create'),
+    path('logout/', logoutview, name='logout'),
+    path('evaluation/<int:pk>/', evaluationview, name='evaluation'),
+]
